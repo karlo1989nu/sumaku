7df98e079a4481c3b4d19da982214e6bf7d78b89
Karlo Navarro <karlo1989nu@gmail.com>
2023-03-21 14:46:34 +0100

    new updates using js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let edad = prompt('Ingresa tu edad:')
+
+let ingresa = edad >= 18;
+
+console.log(ingresa)
+
+ if (edad <18) {
+    prompt("You can only enter the pages allowed for minors");
+ } else {
+    prompt("You can buy catnip");
+ }
