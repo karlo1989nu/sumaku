ab9b6446fb9e1626b75e3a12897e09b68c0239f4
unknown <karlo1989nu@gmail.com>
2023-03-25 19:21:21 +0100

    welcome added new questions related to min age to purchase

--- js/main.js
+++ js/main.js
@@ -7,34 +7,61 @@ Este programa va dará evaluará si:
  
 */
 
-let likeCats = prompt('Do you like cats?: (yes/no)')
 let enteredName = prompt('Enter your name:')
-let enteredAge = prompt('Enter your age:')
+let likeCats = prompt('Do you like cats?: (yes/no)')
+let ingreso = false
+
 console.log(likeCats)
 console.log(enteredName);
-console.log(enteredAge);
 
 if (likeCats === 'yes' || likeCats === 'Yes') {
-   prompt("Welcome to our meownder world, " + enteredName + ". Here you'll find all the Catnip related products you need.");
+   ingreso = true;
+
 } else if (likeCats === 'no' || likeCats === 'No') {
    for (let i = 0; i < 3; i++) {
-      let likeCats1 =  prompt("Are you sure that 'No/no' is your final answer, " + enteredName + "? Please try one more time:")
+      let likeCats1 = prompt("Are you sure that 'No/no' is your final answer, " + enteredName + "? Please try one more time:")
       console.log(likeCats1);
       if (likeCats1 === 'yes' || likeCats1 === 'Yes') {
          likeCats1 = prompt('Please try one more time. Do you like cats?: (yes/no)')
          if (likeCats1 === 'no' || likeCats1 === 'No') {
-            prompt("You don't deserve to live " + enteredName + ", please die soon!")
-            break
+            alert("You don't deserve to live " + enteredName + ", please die soon!")
          } else if (likeCats1 === 'yes' || likeCats1 === 'Yes') {
-            prompt("Welcome to our meownder world, " + enteredName + ". Here you'll find all the Catnip related products you need.");
+            !ingreso
             break
          }
       } else if (likeCats1 === 'no' || likeCats1 === 'No') {
          likeCats1 = prompt('Please try one more time, do you like cats:')
+         if (likeCats1 === 'yes' || likeCats1 === 'Yes') {
+            !ingreso
+            break
+         }
       } else {
          break
       }
    }
-} 
+}
+
+let enteredAge = prompt("Enter your slave's age here:")
+console.log(enteredAge);
+
+alert("Welcome to our meownder world, " + enteredName + ". Here you'll find all the Catnip related products you need.");
+console.log(alert("According to Meowngarian regulations, only Meowngarian Kittizens over 1 cat-years old are allowed to buy and consume catnip therefore, in order to buy in our website, we'll convert your slave's age to cat age."));
+
+if (ingreso) {
+   
+   newCatage = 0;
+
+   if (enteredAge < 40) {
+      newCatage = ((enteredAge - 1) * 3) / 19;
+   } else {
+      newCatage = ((enteredAge - 40) / 4) + 6;
+   }
+   
+   if (newCatage >= 1) {
+      alert("Your slave is " + newCatage + " cat-years old, please proceed to choose your desired products.")
+   } else {
+      alert("Your useless slave can't buy you catnip because its age is " + newCatage + " cat-years old, we recommend to trash it and find another one that can serve you better")
+   }
+}
 
 
