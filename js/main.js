9fc71e08767be43f69933c1fe37ee26dab08e4ac
unknown <karlo1989nu@gmail.com>
2023-03-25 14:02:16 +0100

    new changes related to .js document, implementing new algorithm

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-let edad = prompt('Ingresa tu edad:')
+// Programa de bienvenida al sitio web de BoGi, la Catnip lord
+/*
+Este programa va dará evaluará si:
+ 1. al usuario le gustan los gatos
+ 2. el usuario cuenta con suficiente edad gato para comprar nuestros productos
+ 3. el usuario tiene uso de razón y se arrepiente de no gustarle los gatos
+*/
 
-let ingresa = edad >= 18;
+let likeCats = prompt('Do you like cats?: (yes/no)')
+let nombreingresado = prompt('Enter your name:')
+console.log(likeCats)
+console.log(nombreingresado);
 
-console.log(ingresa)
+if (likeCats === 'yes' || likeCats === 'Yes') {
+   prompt("Welcome to our meownder world, " + nombreingresado +". Here you'll find all the Catnip related products you need.");
+} else {
+   prompt("You don't deserve to live " + nombreingresado + ", die soon!");
+}
 
- if (edad <18) {
-    prompt("You can only enter the pages allowed for minors");
- } else {
-    prompt("You can buy catnip");
- }
