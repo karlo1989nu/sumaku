195a2e194f75c6a3fc99909e873c22766b0439a2
unknown <karlo1989nu@gmail.com>
2023-03-25 15:52:22 +0100

    welcome algorithm created and finished

--- js/main.js
+++ js/main.js
@@ -3,17 +3,38 @@
 Este programa va dará evaluará si:
  1. al usuario le gustan los gatos
  2. el usuario cuenta con suficiente edad gato para comprar nuestros productos
- 3. el usuario tiene uso de razón y se arrepiente de no gustarle los gatos
+ 3. el usuario tiene uso de razón y se arrepiente de no gustarle los gatos después de negarlo max dos veces
+ 
 */
 
 let likeCats = prompt('Do you like cats?: (yes/no)')
-let nombreingresado = prompt('Enter your name:')
+let enteredName = prompt('Enter your name:')
+let enteredAge = prompt('Enter your age:')
 console.log(likeCats)
-console.log(nombreingresado);
+console.log(enteredName);
+console.log(enteredAge);
 
 if (likeCats === 'yes' || likeCats === 'Yes') {
-   prompt("Welcome to our meownder world, " + nombreingresado +". Here you'll find all the Catnip related products you need.");
-} else {
-   prompt("You don't deserve to live " + nombreingresado + ", die soon!");
-}
+   prompt("Welcome to our meownder world, " + enteredName + ". Here you'll find all the Catnip related products you need.");
+} else if (likeCats === 'no' || likeCats === 'No') {
+   for (let i = 0; i < 3; i++) {
+      let likeCats1 =  prompt("Are you sure that 'No/no' is your final answer, " + enteredName + "? Please try one more time:")
+      console.log(likeCats1);
+      if (likeCats1 === 'yes' || likeCats1 === 'Yes') {
+         likeCats1 = prompt('Please try one more time. Do you like cats?: (yes/no)')
+         if (likeCats1 === 'no' || likeCats1 === 'No') {
+            prompt("You don't deserve to live " + enteredName + ", please die soon!")
+            break
+         } else if (likeCats1 === 'yes' || likeCats1 === 'Yes') {
+            prompt("Welcome to our meownder world, " + enteredName + ". Here you'll find all the Catnip related products you need.");
+            break
+         }
+      } else if (likeCats1 === 'no' || likeCats1 === 'No') {
+         likeCats1 = prompt('Please try one more time, do you like cats:')
+      } else {
+         break
+      }
+   }
+} 
+
 
