df3c906bc7bc4090beb0d642b251b5aee02d18aa
unknown <karlo1989nu@gmail.com>
2023-05-18 14:28:28 +0200

    entregafinal

--- js/main.js
+++ js/main.js
@@ -1,111 +1,5 @@
 // Programa de bienvenida al sitio web de BoGi, la Catnip lord
-/*
-Este programa va dará evaluará si:
- 1. al esclavo le gustan los gatos
- 2. el usuario cuenta con suficiente edad gato para comprar nuestros productos
- 3. el usuario tiene uso de razón y se arrepiente de no gustarle los gatos después de negarlo max dos veces
-*/
 
-/*let enteredName = prompt('Enter your name:')
-let likeCats = prompt('Do you like cats?: (yes/no)')
-let ingreso = false
-
-const productsServices = [
-   { id: 101, stock: 112, productname: "Meowloha", price: 500, img: "img1.jpg", description: " Buy it now and taste heaven" },
-   { id: 102, stock: 123, productname: "Meowabonga", price: 1000, img: "img2.jpg", description: " Try this to know the glory" },
-   { id: 103, stock: 134, productname: "Meowrale", price: 1500, img: "img3.jpg", description: "This is a sentence to paradise with tequila" },
-   { id: 104, stock: 145, productname: "Meowla Pirola", price: 2000, img: "img4.jpg", description: "Don't forget your mate with this" },
-   { id: 105, stock: 156, productname: "Meownda la osa", price: 2500, img: "img5.jpg", description: "Available at Moe's" },
-   { id: 106, stock: 167, productname: "Meow", price: 3000, img: "img6.jpg", description: "Meow" },
-   { id: 107, stock: 178, productname: "Meowlala", price: 3500, img: "img7.jpg", description: "Bring your own bagette" },
-   { id: 108, stock: 189, productname: "Meowmamia", price: 4000, img: "img8.jpg", description: "Bring your pizza" },
-   { id: 109, stock: 190, productname: "Meowbye", price: 4500, img: "img9.jpg", description: "Hahahahahahahaha" },
-   { id: 110, stock: 200, productname: "Meowboy", price: 5000, img: "img10.jpg", description: "Meowidy meow" },
-]
-
-console.log(likeCats)
-console.log(enteredName);
-
-if (likeCats === 'yes' || likeCats === 'Yes') {
-   ingreso = true;
-
-} else if (likeCats === 'no' || likeCats === 'No') {
-   let likeCats1 = prompt("We can't accept 'No/no' as the answer for this question, " + enteredName + ". Please try one more time. Do you like cats?: (yes/no)")
-   console.log(likeCats1);
-   for (let i = 0; i < 3; i++) {
-      if (likeCats1 === 'yes' || likeCats1 === 'Yes') {
-         break
-      } else if (likeCats1 === 'no' || likeCats1 === 'No') {
-         likeCats1 = prompt('Please try one more time, do you like cats:')
-      }
-   }
-} else {
-   alert("You can only enter 'Yes', 'yes', 'No', 'no'")
-}
-
-
-let enteredAge = parseInt(prompt("Enter your slave's age here:"))
-console.log(enteredAge);
-
-alert("Welcome to our meownder world, " + enteredName + ". Here you'll find all the Catnip related products you need.");
-alert("According to Meowngarian regulations, only Meowngarian Kittizens over 1 cat-years old are allowed to buy and consume catnip therefore, in order to buy in our website, we'll convert your slave's age to cat age.")
-
-let newCatage = 0;
-
-function catAge() {
-
-   if (enteredAge < 40) {
-      newCatage = ((enteredAge - 1) * 3) / 19;
-   } else {
-      newCatage = ((enteredAge - 40) / 4) + 6;
-   }
-
-}
-
-catAge(enteredAge)
-
-if (ingreso) {
-
-   if (newCatage >= 1) {
-      alert("Your slave is " + Math.round(newCatage) + " cat-years old, please proceed to choose your desired products.")
-   } else {
-      alert("Your useless slave can't buy you catnip because its age is " + Math.round(newCatage) + " cat-years old, we recommend to trash it and find another one that can serve you better")
-   }
-}*/
-
-/* II Entrega => A partir de esta línea estaré escribiendo el código perteneciente a la segunda entrega de la comisión #52125. */
-
-// Lista de productos de la Catnip tienda (aray)
-
-/*let prodSerNew = []
-
-for (let i in productsServices) {
-   i += "_Catnip: " + productsServices[i].productname + " Stock: " + productsServices[i].stock
-   prodSerNew.push(i);
-}
-
- alert("This is our list of products: " + prodSerNew)
-
- let productsearch = prompt("Please type your desired product from the list")
-
-const findProduct = productsServices.find((el) => el.productname == productsearch)
-
- Test: console.log(findProduct);
- if (findProduct) {
-    alert(`Your selected product is ${findProduct.productname} whose price is $${findProduct.price}. ${findProduct.description}. If this information is correct please proceed to pay your purchase.`)   
- } else {
-    alert("The product selected isn't available.")  
- }
-const totalStock = productsServices.reduce((acc, el) => {
-   return acc + el.stock
-}, 0)
-
-console.log(totalStock);
-console.dir(document.body)
-console.dir(document.head)*/
-
-/*III Entrega => En esta entrega construí la página STORE en la que estaré publicando una lista dinámica
-de productos y ya no por medio de alert.*/
 const cards = document.getElementById('cards')
 const items = document.getElementById('items')
 const footer = document.getElementById('footer')
@@ -118,17 +12,24 @@ let cart  = {}
 
 document.addEventListener('DOMContentLoaded', () => {
    fetchData()
+   if(localStorage.getItem('carrito')){
+      cart = JSON.parse(localStorage.getItem('carrito'))
+      pintarCarrito()
+   }
 })
 
 cards.addEventListener('click', e => {
    addCarrito(e)
 })
 
+items.addEventListener('click', e => {
+   btnAccion(e)
+})
+
 const fetchData = async () => {
    try {
       const res = await fetch('../json/api.json')
       const data = await res.json()
-      //console.log(data);
       printCard(data)
    } catch (error) {
       console.log(error);
@@ -149,8 +50,6 @@ const printCard = data => {
 }
 
 const addCarrito = e => {
-   // console.log(e.target);
-   // console.log(e.target.classList.contains('btn'));
    if (e.target.classList.contains('btn')) {
       setCart(e.target.parentElement)
    }
@@ -158,7 +57,7 @@ const addCarrito = e => {
 }
 
 const setCart = objeto => {
-   //console.log(objeto);
+
    const producto = {
       id: objeto.querySelector('.btn').dataset.id,
       productname: objeto.querySelector('h5').textContent,
@@ -175,7 +74,7 @@ const setCart = objeto => {
 }
 
 const pintarCarrito = () => {
-   console.log(cart)
+   items.innerHTML=""
    Object.values(cart).forEach(producto => {
       templateCarrito.querySelector('th').textContent = producto.id
       templateCarrito.querySelectorAll('td')[0].textContent = producto.productname
@@ -188,4 +87,56 @@ const pintarCarrito = () => {
       fragment.appendChild(clone)
    })
    items.appendChild(fragment)
+
+   pintarFooter()
+
+   localStorage.setItem('carrito', JSON.stringify(cart))
+}
+
+const pintarFooter = () => {
+   footer.innerHTML = ''
+   if (Object.keys(cart).length === 0) {
+      footer.innerHTML = `
+      <th scope="row" colspan="5">Empty cart - start buying!</th>
+      `
+      return
+   }
+
+   const nCantidad = Object.values(cart).reduce((acc,{ cantidad }) => acc + cantidad, 0)
+   const nPrecio = Object.values(cart).reduce((acc, { cantidad, price }) => acc + cantidad * price, 0)
+
+   templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+   templateFooter.querySelector('span').textContent = nPrecio
+
+   const clone = templateFooter.cloneNode(true)
+   fragment.appendChild(clone)
+   footer.appendChild(fragment)
+
+   const btnVaciar = document.getElementById('.vaciar-carrito')
+   btnVaciar.addEventListener('click', () => {
+      cart = {}
+      printCard()
+   })
+} 
+
+const btnAccion = e => {
+   console.log(e.target);
+   if(e.target.classList.contains('btn-info')){
+      cart[e.target.dataset.id]
+      const producto = cart[e.target.dataset.id]
+      producto.cantidad++
+      cart[e.target.dataset.id] = {...producto}
+
+      printCard()
+   }
+
+   if(e.target.classList.contains('btn-danger')){
+      const producto = cart[e.target.dataset.id]
+      producto.cantidad--
+      if(producto.cantidad===0){
+         delete cart[e.target.dataset.id]
+      }
+      printCard()
+   }
+   e.stopPropagation()
 }
